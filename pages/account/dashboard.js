9ea75b1f3e2c5c74ee1083e23343ec9e7391678f
siavashsajady <hubgitpro@gmail.com>
2022-01-09 18:00:44 +0100

    create helper function to parse the cookie &  get employees for dashboard

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,31 @@
+import { parseCookies } from '@/helpers/index';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function DashboardPage() {
+export default function DashboardPage({ employees }) {
+  console.log(employees);
   return (
     <Layout title='User Dashboard'>
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/employees/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const employees = await res.json();
+
+  return {
+    props: {
+      employees,
+    },
+  };
 }
